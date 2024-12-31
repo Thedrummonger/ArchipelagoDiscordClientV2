a6076d707046c1db660661cf2c428ef84b89b099
Thedrummonger <drummerxtmail@gmail.com>
2024-12-31 15:50:29 -0600

    Since archipelagos SocketClosed event doesn't seem to trigger when the server is closed, run some gross code to check the servers status every few seconds.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using static ArchipelagoDiscordClientLegacy.Data.DiscordBotData;
 using ArchipelagoDiscordClientLegacy.Data;
 using TDMUtils;
+using ArchipelagoDiscordClientLegacy.Helpers;
 
 namespace ArchipelagoDiscordClient
 {
@@ -38,9 +39,29 @@ namespace ArchipelagoDiscordClient
             //Run a background task to constantly send messages in the send queue
             _ = Task.Run(BotClient.MessageQueueHandler.ProcessMessageQueueAsync);
 
+            _ = Task.Run(() => CheckServerLife(BotClient));
+
             //TODO, maybe just replace this with a loop to handle console commands on the server it's self
             await Task.Delay(-1);
         }
 
+        private void CheckServerLife(DiscordBot bot)
+        {
+            //Archipelagos SocketClosed event doesn't seem to trigger when the server is closed?
+            //for now we'll just check the connections manually every few seconds.
+            while (true) 
+            { 
+                foreach(var i in bot.ActiveSessions)
+                {
+                    try { i.Value.archipelagoSession.DataStorage.GetClientStatus(); }
+                    catch
+                    {
+                        _ = archipelagoConnectionHelpers.CleanAndCloseChannel(bot, i.Key);
+                        bot.QueueMessage(i.Value.DiscordChannel!, $"Connection closed:\nServer no longer reachable");
+                    }
+                }
+                Task.Delay(5000);
+            }
+        }
     }
 }

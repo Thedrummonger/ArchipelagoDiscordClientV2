db7cb0dc93e0ce4b58681699164e810675f41406
thedrummonger <drummerxtmail@gmail.com>
2025-02-04 14:00:50 -0600

    Update the project structure so we can include the Dev Client in git

---
+++ DevClient/Commands/ConsoleCommands/DebugConsoleCommands.cs
@@ -0,0 +1,31 @@
+﻿using ArchipelagoDiscordClientLegacy.Commands.ConsoleCommands;
+using ArchipelagoDiscordClientLegacy.Data;
+using ArchipelagoDiscordClientLegacy.Handlers;
+
+namespace DevClient.Commands.ConsoleCommands
+{
+    internal class DebugConsoleCommands
+    {
+        public class ShowHeartbeatConsoleCommand : IConsoleCommand
+        {
+            public string Name => "show_heartbeat";
+
+            public void ExecuteCommand(DiscordBotData.DiscordBot discordBot)
+            {
+                ArchipelagoDiscordClientLegacy.Program.ShowHeartbeat = !ArchipelagoDiscordClientLegacy.Program.ShowHeartbeat;
+                Console.WriteLine($"Showing Heartbeat: {ArchipelagoDiscordClientLegacy.Program.ShowHeartbeat}");
+            }
+        }
+
+        public class HelpConsoleCommand : IConsoleCommand
+        {
+            public string Name => "help";
+
+            public void ExecuteCommand(DiscordBotData.DiscordBot discordBot)
+            {
+                var t = ConsoleCommandHandlers.ConsoleCommandRegistry.Keys.ToArray();
+                Console.WriteLine($"available commands:\n{string.Join("\n", t)}");
+            }
+        }
+    }
+}
---
+++ DevClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Discord;
+using static ArchipelagoDiscordClientLegacy.Data.DiscordBotData;
+
+namespace DevClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            BotSocketConfig.SetLogLevel(LogSeverity.Debug);
+            await ArchipelagoDiscordClientLegacy.Program.RunBotAsync();
+        }
+    }
+}
